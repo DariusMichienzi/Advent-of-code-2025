4ce45cf29bc0c831a9896e0ec97b3d2f4741175f
Darius Michienzi <er19801@bristol.ac.uk>
2025-12-01 09:07:35 +0000

    added day 01 solution

--- Day01.jl
+++ Day01.jl
@@ -1,25 +1,38 @@
-f = "./Day01.txt"
-lines = parse.(Int,readlines(f))
+f = "./DataFiles/Day01.txt"
+lines = readlines(f)
 
-for line in eachindex(lines)
-    C = 2020 - line
-    if isempty(findall(x->x==C,lines)) == false
-        global ans1 = line * C
+direction = []
+numbers = []
+for line in lines
+    push!(direction,line[1])
+    push!(numbers,line[2:end])
+end
+numbers = parse.(Int,numbers)
+
+pos = [50]
+for i in eachindex(numbers)
+    if direction[i] == 'L'
+        push!(pos, mod(pos[i] - numbers[i] , 100))
+    elseif direction[i] == 'R'
+        push!(pos, mod(pos[i] + numbers[i] , 100))
+    end
+end
+ans1 = length(findall(x->x==0,pos))
+println("Part 1 answer = $ans1")
+
+#end of part 1
+
+pos = [50]
+visit = []
+for i in eachindex(numbers)
+    if direction[i] == 'L'
+        push!(pos, mod(pos[i] - numbers[i] , 100))
+        visit = [visit;mod.(collect(pos[i]:-1:pos[i] - numbers[i]),100)]
+    elseif direction[i] == 'R'
+        visit = [visit;mod.(collect(pos[i]:1:pos[i] + numbers[i]),100)]
+        push!(pos, mod(pos[i] + numbers[i] , 100))
     end
 end
 
-println("part 1 answer = $ans1")
-
-##end of part 1 
-
-for i in eachindex(lines)
-    for j in eachindex(lines)
-        for k in eachindex(lines)
-            if i!=j&& j!=k && lines[i] + lines[j] + lines[k] == 2020
-                global ans2 = lines[i] * lines[j] * lines[k]
-            end
-        end
-    end
-end
-
-println("part 2 answer = $ans2")
+ans2 = length(findall(x->x==0,visit)) - length(findall(x->x==0,pos))
+println("Part 2 answer = $ans2")
