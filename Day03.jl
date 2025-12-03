8e7e391df9ec3f2080c1793e79f8e3f80aa405e0
Darius Michienzi <er19801@bristol.ac.uk>
2025-12-03 10:14:50 +0000

    added Day03 solution

--- Day03.jl
+++ Day03.jl
@@ -1,2 +1,32 @@
 f = "./DataFiles/Day03.txt"
 lines = readlines(f)
+ans1 = 0
+
+for line in lines 
+line = parse.(Int,split(line,""))
+    combinations = Int[]
+for i in 1:(length(line)-1)
+    push!(combinations,10*line[i]+maximum(line[i+1:end]))
+end
+
+ans1 += maximum(combinations)
+end
+
+println("Part 1 answer = $ans1")
+
+# end of part 1
+
+ans2 = 0
+N_dig = 12
+for line in lines 
+    total = 0
+    line = parse.(Int,split(line,""))
+    index = 0
+    for i in (N_dig-1):-1:0
+        index += argmax(line[index+1: end-i])
+        total = parse(Int,string(total,line[index]))
+    end
+    ans2 += total
+end
+
+println("Part 2 answer = $ans2")
